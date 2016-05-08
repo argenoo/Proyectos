dc927c5b0a240c48399fe9b5407384b3e786335d
Argeno <argenoo@me.com>
2016-05-08 00:57:00 -0500

    Initial Commit

---
+++ PedirPizzaAppleWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  PedirPizzaAppleWatch WatchKit Extension
+//
+//  Created by Argenis García Jiménez on 08/05/16.
+//  Copyright © 2016 Argenis García Jiménez. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ PedirPizzaAppleWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PedirPizzaAppleWatch
+//
+//  Created by Argenis García Jiménez on 08/05/16.
+//  Copyright © 2016 Argenis García Jiménez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
