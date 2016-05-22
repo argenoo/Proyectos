ee5dd32163d97646e5ba4ed2534fabfbff3b8eb7
Argeno <argenoo@me.com>
2016-05-22 09:40:43 -0500

    Initial Commit

---
+++ ServidorOpenlIBRARY/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ServidorOpenlIBRARY
+//
+//  Created by Argenis García Jiménez on 22/05/16.
+//  Copyright © 2016 Argenis García Jiménez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
