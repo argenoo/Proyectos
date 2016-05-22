50b0f23e49e7285a41e65b2af43cacfc09fb960c
Argeno <argenoo@me.com>
2016-05-22 10:28:07 -0500

    Proyecto de prueba de servidor

--- ServidorOpenlIBRARY/ViewController.swift
+++ ServidorOpenlIBRARY/ViewController.swift
@@ -9,7 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ 
+     
+     @IBOutlet weak var CajaISBN: UITextField!
+    
+     
+     @IBAction func Busqueda(sender: AnyObject) {
+    sincrono()
+     }
+     
+     
+     @IBAction func Limpiar(sender: AnyObject) {
+        CajaResultado.text = ""
+        CajaISBN.text = ""
+     }
+     
+     
+     @IBOutlet weak var CajaResultado: UITextView!
+    
+    
+    func sincrono () {
+        var resultadoTexto : String = ""
+        let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
+        resultadoTexto = String(urls)
+        resultadoTexto = resultadoTexto + CajaISBN.text!
+        let url = NSURL(string : resultadoTexto)
+        let datos : NSData? = NSData(contentsOfURL: url!)
+        let texto = NSString(data:datos!, encoding:NSUTF8StringEncoding)
+        
+        if texto! == "{}" {
+    CajaResultado.text = "EL SERVIDOR ESTA EN BLANCO"
+    }
+        else{
+        CajaResultado.text = String(texto!)
+        }
+    }
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
